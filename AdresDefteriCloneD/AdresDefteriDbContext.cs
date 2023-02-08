17a73e7c5425df0ff9e4d69529cfd51a4e8dc2ad
dogukanonay3 <donay3307@gmail.com>
2023-02-09 02:07:38 +0300

    Add project files.

---
+++ AdresDefteriCloneD/AdresDefteriDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdresDefteriCloneD
+{
+    internal class AdresDefteriDbContext : DbContext
+    {
+        public DbSet<Musteri> Musteri { get; set; }
+        public DbSet<Personel> Personel { get; set; }
+        public DbSet<Paydas> Paydas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-OR82PAL\\SQLEXPRESS01;Database=AdresDefteri;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personel>()
+                .Property(s => s.Id)
+                .UseIdentityColumn();
+
+            modelBuilder.Entity<Paydas>()
+               .Property(s => s.Id)
+               .UseIdentityColumn();
+
+            modelBuilder.Entity<Musteri>()
+               .Property(s => s.Id)
+               .UseIdentityColumn();
+
+        }
+
+
+    }
+}
